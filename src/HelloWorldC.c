e92644fb5f9e47ecf2c88c527857cef30f8c9c7d
pietersp <pietersp@gmail.com>
2013-02-23 23:24:00 +0200

    Make loop of hello world strings

--- src/HelloWorldC.c
+++ src/HelloWorldC.c
@@ -13,8 +13,11 @@
 
 // Main method that gets executed on startup
 int main(void) {
+	int i;
+	for (i = 0; i < 10; i++){
+		puts("Hello World ");
+	}
 
-	puts("Hello World 1 2 3");
 	puts("Bye World");
 	return EXIT_SUCCESS;
 }
