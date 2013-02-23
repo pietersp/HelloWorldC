d90a7e817927b3a5097fb1f955e323e4fda44cb6
pietersp <pietersp@gmail.com>
2013-02-23 23:51:27 +0200

    Changed to stars framing

--- src/HelloWorldC.c
+++ src/HelloWorldC.c
@@ -13,11 +13,11 @@
 
 // Main method that gets executed on startup
 int main(void) {
-	int i;
-	for (i = 0; i < 10; i++){
-		puts("Hello World ");
-	}
 
+	puts("***********");
+	puts("Hello World");
+	puts("***********");
 	puts("Bye World");
+	puts("***********");
 	return EXIT_SUCCESS;
 }
